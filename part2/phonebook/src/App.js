ce676f9b96c608e5659be49ec11ab8592cc6fc90
Wrik Das <wrikdas22@gmail.com>
2022-03-29 23:33:02 +0530

    completed 2.6-2.10

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+
+const Filter = ({ filter, handleFilterChange }) => {
+  return (
+    <div>
+      filter shown with <input value={filter} onChange={handleFilterChange}/>
+    </div>
+  )
+}
+
+const PersonForm = (props) => {
+  return (
+    <form onSubmit={props.handleAddContact}>
+      <div>name: <input value={props.newName} onChange={props.handleNameChange}/></div>
+      <div>number: <input value={props.newNumber} onChange={props.handleNumberChange}/></div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Person = ({ name, number }) => {
+  return <div>{name} {number}</div>
+}
+
+const Persons = ({ persons }) => {
+  return (
+    <div>
+      {persons.map(({ name, number }) => {
+        return <Person key={name} name={name} number={number}/>
+      })}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNumber(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
+  const handleAddContact = (event) => {
+    event.preventDefault()
+    if (persons.find(({ name }) => name === newName)) {
+      return alert(`${newName} is already added to phonebook`)
+    }
+    setPersons(persons.concat({ name: newName, number: newNumber }))
+    setNewName('')
+    setNumber('')
+  }
+
+  const personsToShow = filter === ''
+    ? persons
+    : persons.filter((person) => person.name.includes(filter))
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <Filter filter={filter} handleFilterChange={handleFilterChange}/>
+      <h2>Add a new</h2>
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        handleAddContact={handleAddContact}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={personsToShow}/>
+    </div>
+  )
+}
+
+export default App
