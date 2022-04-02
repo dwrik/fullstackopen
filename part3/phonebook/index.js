7f92ee7c4ba1010a4cbd2ae98645804895cd5c40
Wrik Das <wrikdas22@gmail.com>
2022-04-02 23:47:35 +0530

    completed 3.13-3.14

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/Person')
 
 const app = express()
 
@@ -11,35 +13,37 @@ app.use(express.static('build'))
 morgan.token('body', (request, response) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+// let persons = [
+//   { 
+//     "id": 1,
+//     "name": "Arto Hellas", 
+//     "number": "040-123456"
+//   },
+//   { 
+//     "id": 2,
+//     "name": "Ada Lovelace", 
+//     "number": "39-44-5323523"
+//   },
+//   { 
+//     "id": 3,
+//     "name": "Dan Abramov", 
+//     "number": "12-43-234345"
+//   },
+//   { 
+//     "id": 4,
+//     "name": "Mary Poppendieck", 
+//     "number": "39-23-6423122"
+//   }
+// ]
 
-const generateId = () => {
-  return Math.floor(Math.random() * 99999)
-}
+// const generateId = () => {
+//   return Math.floor(Math.random() * 99999)
+// }
 
 app.get('/api/persons', (request, response) => {
-  response.send(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -59,31 +63,23 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 app.post('/api/persons', (request, response) => {
-  const data = request.body
-  if (!data.name || !data.number) {
+  const { name, number } = request.body
+  if (!name || !number) {
     return response.status(400).send({
       error: 'name or number missing'
     })
   }
-  if (persons.find(person => person.name.toLowerCase() === data.name.toLowerCase())) {
-    return response.status(400).send({
-      error: 'name must be unique'
-    })
-  }
-  const person = {
-    id: generateId(),
-    name: data.name,
-    number: data.number,
-  }
-  persons = persons.concat(person)
-  response.send(person)
+  const person = new Person ({ name, number })
+  person.save().then(saved => {
+    response.json(saved)
+  })
 })
 
 app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people<p>${new Date()}`)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
 })
