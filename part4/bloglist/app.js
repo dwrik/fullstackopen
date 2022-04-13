dda8632fdf4a654aa691c7bcea7f088df8693df8
Wrik Das <wrikdas22@gmail.com>
2022-04-13 23:15:10 +0530

    completed 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blog')
+const usersRouter = require('./controllers/user')
 const middleware = require('./utils/middleware')
 require('express-async-errors')
 
@@ -25,6 +26,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
